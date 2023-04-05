0ff8dad47777a83868e72020bac422705887fca0
po4tion <po4tion0429@gmail.com>
2023-04-05 16:35:31 +0900

    Solved

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,9 @@
-type MyAwaited<T> = any
+type IsPromise<P> = P extends Promise<infer K> ? IsPromise<K> : P
+
+type MyAwaited<T extends Promise<any> | PromiseLike<any>> = T extends Promise<
+  infer P
+>
+  ? IsPromise<P>
+  : T extends { then: (onfulfilled: (arg: infer K) => any) => any }
+  ? K
+  : never
