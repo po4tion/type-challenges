9d7c05a82021ec20932164f7ec7be9b5dc4597bb
po4tion <po4tion0429@gmail.com>
2023-04-10 11:39:14 +0900

    Solved

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,11 @@
-type Includes<T extends readonly any[], U> = any
+type MyEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+
+type Includes<T extends any[], U> = T extends [infer First, ...infer Rest]
+  ? MyEqual<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
